cd65b39a6110eab19b1d9cf95dc33a1b13940f75
Gabriel Garcia Pimentel Mendonça <gabriel.garcia@ligainvest.com.br>
2023-02-23 14:56:46 -0300

    First commit

---
+++ android/app/src/main/java/com/walmartlabs/ern/agoraminiapp/RunnerConfig.java
@@ -0,0 +1,17 @@
+package com.walmartlabs.ern.agoraminiapp;
+
+import com.walmartlabs.ern.container.miniapps.AgoraMiniappActivity;
+
+//
+// GENERATED CODE: DO NOT MODIFY
+//
+// Do not modify the content of this file as it will be regenerated
+// every time a run-android command is executed.
+// See https://native.electrode.io/cli-commands/run-android
+
+final class RunnerConfig {
+    static final Class MAIN_MINIAPP_ACTIVITY_CLASS = AgoraMiniappActivity.class;
+    static final boolean RN_DEV_SUPPORT_ENABLED = true;
+    static final String PACKAGER_HOST = "localhost";
+    static final String PACKAGER_PORT = "8081";
+}
